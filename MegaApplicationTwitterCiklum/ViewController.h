9d32422b434f3f4983f7ab2d6b9133c2de319970
Alexandr Lobanov <lobanov92@gmail.com>
2016-01-22 12:38:37 +0200

    Initial Commit

---
+++ MegaApplicationTwitterCiklum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MegaApplicationTwitterCiklum
+//
+//  Created by Alexandr Lobanov on 1/22/16.
+//  Copyright © 2016 Alexandr Lobanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MegaApplicationTwitterCiklum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MegaApplicationTwitterCiklum
+//
+//  Created by Alexandr Lobanov on 1/22/16.
+//  Copyright © 2016 Alexandr Lobanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
